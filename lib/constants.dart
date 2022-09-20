1ffbf4e29e6dc181e111ad25d4e7a2f0e8eb4fa3
Ekojode Oma-Victor <ekojodeoma@gmail.com>
2022-09-20 13:12:38 +0100

    Ugraded to flutter 3.2

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color purple = Color(0xff5243D2);
+const Color deepPurple = Color(0xff0D073C);
+const Color pink = Color(0xffE4325A);
+const Color red = Color(0xffA61818);
+const Color orange = Color(0xffFF5F00);
